78978a04364e936bb98c523fc7d471f5023525d8
IsaacOldenburg <98563702+IsaacOldenburg@users.noreply.github.com>
2026-06-23 13:49:23 -0400

    Creating script to deploy the application to the Pi's.

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+laptopIP = "192.168.0.190"
+laptopUser = "IsaacO_P16"
+laptopPath = "\Users\iolde\iCloud\iCloudDrive\School\Research\DrGeordon\Programs\RouterScripts"
+piIP = ("192.168.0.244" "192.168.0.198")
+
+echo "Staging and committing changes to GitHub"
+git add .
+git commit -m "Updating Pi scripts"
+
+for ip in ${piIP[@]}; do
+    echo "Deploying to $ip"
+    ssh pi@$ip "cd /home/RouterScripts && git pull ssh://laptopUser@$laptopIP$laptopPath main"
+done
