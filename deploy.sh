0efb12d20645743c5752a8cde8b9a163844b781d
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-06-24 14:47:54 -0400

    Updating Pi scripts

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ laptopIP="192.168.0.190"
 laptopUser="IsaacO_P16"
 laptopPath="/mnt/c/Users/iolde/iCloud/iCloudDrive/School/Research/DrGeordon/Programs/RoutingScripts"
 piIP=("192.168.0.244" "192.168.0.198")
-piPath="/home/pi/RoutingScripts/"
+piPath="/home/pi/scripts/"
 piUser = "pi"
 
 echo "Staging and committing changes to GitHub"
