54c590979308367add80a0d926a91bd3dd46f226
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-06-25 11:41:22 -0400

    Updating Pi scripts

--- Latency.py
+++ Latency.py
@@ -50,6 +50,7 @@ def main():
     init_tc_interface()
     
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     sock.bind((UDP_IP, UDP_PORT))
     print(f"Pi Agent online. Awaiting variable pushes from laptop...\n")
 
