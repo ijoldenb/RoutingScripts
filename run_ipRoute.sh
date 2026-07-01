f9be0a3c79118bcdadbf560949ddea3718ec7aef
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-01 14:09:26 -0400

    Updating Pi scripts

---
+++ run_ipRoute.sh
@@ -0,0 +1,10 @@
+piIP=("192.168.0.244" "192.168.0.198" "192.167.0.237")
+scriptPath="/home/pi/scripts/RoutingScripts/ipRoute.sh"
+
+for host in ${piIP[@]}; do
+    ssh -n "pi@$host" "./$scriptPath > /dev/null 2>&1 &"
+    done
+
+    echo "success on $host"
+
+echo "Commands Sent"
