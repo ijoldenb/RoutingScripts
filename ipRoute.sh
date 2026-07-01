52d7df6858bdb704d3a2210db79119f885c8f3ab
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-01 14:31:18 -0400

    Updating Pi scripts

--- ipRoute.sh
+++ ipRoute.sh
@@ -1,7 +1,8 @@
 local_ip = $(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 for i in {101..120}; do
-    if [ "192.168.$i.0" = "$local_ip" ]; then
+    if ["$local_ip" = "192.168.$i.0"]; then
+        echo "Skipping local subnet interface: 192.168.$i.0/24"
         continue
     else
         ip route add 192.168.$i.0/24 dev eth0
