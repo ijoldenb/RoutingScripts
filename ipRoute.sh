f99837d2e9d06b07c038cdc68b8c1a8b9062c9ff
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-01 14:12:33 -0400

    Updating Pi scripts

--- ipRoute.sh
+++ ipRoute.sh
@@ -1,7 +1,7 @@
 local_ip = $(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 for i in {101..120}; do
-    if i = $local_ip; then
+    if [ "192.168.$i.0" = "$local_ip" ]; then
         continue
     else
         ip route add 192.168.$i.0/24 dev eth0
