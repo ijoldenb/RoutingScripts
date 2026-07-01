e79a93fde39ba7d2279be5e226849570c87cbff4
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-01 14:32:12 -0400

    Updating Pi scripts

--- ipRoute.sh
+++ ipRoute.sh
@@ -1,4 +1,4 @@
-local_ip = $(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+local_ip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 for i in {101..120}; do
     if ["$local_ip" = "192.168.$i.0"]; then
