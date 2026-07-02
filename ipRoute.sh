3bd84f8a5302fb0727a4ed2eccad98c7d5e87c8a
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-02 09:19:44 -0400

    Updating Pi scripts

--- ipRoute.sh
+++ ipRoute.sh
@@ -1,12 +1,13 @@
 local_ip=$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
 
 for i in $(seq 101 120); do
+    currentIPSubnet="192.168.$i.0"
     currentIP="192.168.$i.10"
     if [ "$local_ip" = "$currentIP" ]; then
-        echo "Skipping local subnet interface: $currentIP/24"
+        echo "Skipping local subnet interface: $currentIPSubnet/24"
         continue
     else
-        ip route add "$currentIP/24" dev eth0
-        echo "Added route for $currentIP/24 via eth0"
+        ip route add "$currentIPSubnet/24" dev eth0
+        echo "Added route for $currentIPSubnet/24 via eth0"
     fi
 done
