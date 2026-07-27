6c626f1bf5ceee02ed6cb731a0171cccc61a6594
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-27 15:14:43 -0400

    Updating Pi scripts

--- run_packetTracer.sh
+++ run_packetTracer.sh
@@ -1,5 +1,5 @@
 piIP=("192.168.0.244" "192.168.0.198" "192.168.0.237" "192.168.0.129")
-scriptPath="/home/pi/scripts/RoutingScripts/packetTracer.py"
+scriptPath="/home/pi/scripts/packetTracer.py"
 
 for host in ${piIP[@]}; do
     ssh -n "pi@$host" "sudo nohup python3 $scriptPath > /dev/null 2>&1 &"
