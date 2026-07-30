c31a2d1d3f00d7783a6c7065771f4ddf63de3d6d
Isaac Oldenburg <ijoldenb@mtu.edu>
2026-07-30 11:50:51 -0400

    Updating Pi scripts

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 laptopIP="192.168.0.243"
 laptopUser="IsaacO_P16"
 laptopPath="/home/ijoldenb/RoutingScripts/"
-piIP=("192.168.0.244" "192.168.0.198" "192.168.0.237" "192.168.0.129")
 piPath="/home/pi/scripts/"
+
+# Define path to the centralized configuration file
+CLUSTER_CONFIG="${laptopPath}control_IP.yaml"
+
+# Dynamically parse out the IP addresses from the PI_CLUSTER dictionary format
+mapfile -t piIP < <(grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' "$CLUSTER_CONFIG")
 
 echo "Staging and committing changes to GitHub"
 git add .
